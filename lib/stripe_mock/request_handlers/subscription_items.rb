103a859b134cca21718077730df16761d648c41a
Rick Benavidez <rick.benavidez@gmail.com>
2018-08-29 17:48:14 +0000

    Funcioning subscription item endpoints. - Stripe::SubscriptionItem.create   Endpoint to Add new items to the current subscription item list
    - Stripe::SubscriptionItem.list
      Endpoint to retrieve the existing items for a subscription
    
    - Stripe::SubscriptionItem.update
      Endpoint to save changed items for a subscription
    
    (Please note the naive implementation of the internals which
    are likely not correct and no supporting tests.)

--- lib/stripe_mock/request_handlers/subscription_items.rb
+++ lib/stripe_mock/request_handlers/subscription_items.rb
@@ -3,17 +3,59 @@ module StripeMock
     module SubscriptionItems
 
       def SubscriptionItems.included(klass)
-        klass.add_handler 'get /v1/subscription_items', :retrieve_subscription_items
+        klass.add_handler 'get /v1/subscription_items', :list_subscription_items
         klass.add_handler 'post /v1/subscription_items', :create_subscription_items
+        klass.add_handler 'post /v1/subscription_items/(.*)', :update_subscription_items
       end
 
-      def retrieve_subscription_items(route, method_url, params, headers)
+      def list_subscription_items(route, method_url, params, headers)
         route =~ method_url
-        Data.mock_list_object(subscriptions_items, params)
+
+        subscription_id = params[:subscription][:id]
+        subscription = assert_existence :subscription, subscription_id, subscriptions[subscription_id]
+        verify_active_status(subscription)
+        Data.mock_list_object(subscription[:items][:data], params)
+      end
+
+      def update_subscription_items(route, method_url, params, headers)
+        route =~ method_url
+
+        subscription_item_id = $1
+        subscription_id = subscriptions.keys.first
+
+        subscription = assert_existence :subscription, subscription_id, subscriptions[subscription_id]
+        verify_active_status(subscription)
+
+        customer_id = subscription[:customer]
+        customer = assert_existence :customer, customer_id, customers[customer_id]
+
+        subscription_item = subscription[:items][:data].find { |si| si[:id] == subscription_item_id }
+        subscription[:items][:data].delete_if { |si| si[:id] == subscription_item_id }
+        new_item = Data.mock_subscription_item(subscription_item.merge({ 'quantity': params[:quantity]}))
+        subscription[:items][:data] << new_item
+
+        subscriptions[subscription[:id]] = subscription
+        subscription_item
       end
 
       def create_subscription_items(route, method_url, params, headers)
-        {}
+        route =~ method_url
+        subscription_id = params[:subscription][:id]
+        subscription = assert_existence :subscription, subscription_id, subscriptions[subscription_id]
+        verify_active_status(subscription)
+
+        customer_id = subscription[:customer]
+        customer = assert_existence :customer, customer_id, customers[customer_id]
+
+        subscription_plans = get_subscription_plans_from_params(params)
+
+        new_item = Data.mock_subscription_item( id: "#{params[:plan]}-#{rand(10000)}", plan: { id: params[:plan] }, quantity: params[:quantity], metadata: params[:metadata])
+        subscription[:items][:data] << new_item
+
+        subscriptions[subscription[:id]] = subscription
+        add_subscription_to_customer(customer, subscription)
+
+        subscriptions[subscription[:id]]
       end
     end
   end
