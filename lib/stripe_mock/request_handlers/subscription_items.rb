4cd078f84ac2fec59242a50fe6fe41250c2bcd50
Rick Benavidez <rick.benavidez@gmail.com>
2020-08-15 02:52:16 +0000

    Merging upstream

--- lib/stripe_mock/request_handlers/subscription_items.rb
+++ lib/stripe_mock/request_handlers/subscription_items.rb
@@ -3,7 +3,7 @@ module StripeMock
     module SubscriptionItems
 
       def SubscriptionItems.included(klass)
-        klass.add_handler 'get /v1/subscription_items', :retrieve_subscription_items
+        klass.add_handler 'get /v1/subscription_items', :list_subscription_items
         klass.add_handler 'post /v1/subscription_items', :create_subscription_items
         klass.add_handler 'post /v1/subscription_items/(.*)', :update_subscription_items
       end
@@ -13,31 +13,14 @@ module StripeMock
 
         require_param(:subscription) unless params[:subscription]
 
-        Data.mock_list_object(subscriptions_items, params)
-      end
-
-      def create_subscription_items(route, method_url, params, headers)
-        params[:id] ||= new_id('si')
-
-        require_param(:subscription) unless params[:subscription]
-        require_param(:plan) unless params[:plan]
-
-        subscriptions_items[params[:id]] = Data.mock_subscription_item(params.merge(plan: plans[params[:plan]]))
-      end
-
-      def update_subscription_item(route, method_url, params, headers)
-        route =~ method_url
-
-        subscription_item = assert_existence :subscription_item, $1, subscriptions_items[$1]
-        subscription_item.merge!(params.merge(plan: plans[params[:plan]]))
-
         subscription_id = params[:subscription][:id]
         subscription = assert_existence :subscription, subscription_id, subscriptions[subscription_id]
         verify_active_status(subscription)
+
         Data.mock_list_object(subscription[:items][:data], params)
       end
 
-      def update_subscription_items(route, method_url, params, headers)
+      def update_subscription_item(route, method_url, params, headers)
         route =~ method_url
 
         subscription_item_id = $1
